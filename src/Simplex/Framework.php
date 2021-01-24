fd622e62a7005daeef332b11c7c8de928a61d95b
lincongcong <906561974@qq.com>
2021-01-24 20:52:37 +0800

    0.1.2 rely on interfaces instead of concrete classes

--- src/Simplex/Framework.php
+++ src/Simplex/Framework.php
@@ -5,9 +5,12 @@ namespace Simplex;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Controller\ArgumentResolver;
+use Symfony\Component\HttpKernel\Controller\ArgumentResolverInterface;
 use Symfony\Component\HttpKernel\Controller\ControllerResolver;
+use Symfony\Component\HttpKernel\Controller\ControllerResolverInterface;
 use Symfony\Component\Routing\Exception\ResourceNotFoundException;
 use Symfony\Component\Routing\Matcher\UrlMatcher;
+use Symfony\Component\Routing\Matcher\UrlMatcherInterface;
 
 class Framework
 {
@@ -15,10 +18,10 @@ class Framework
     private $controllerResolver;
     private $argumentResolver;
 
-    public function __construct($matcher, $controllerResolver, $argumentResolver)
+    public function __construct(UrlMatcherInterface $matcher, ControllerResolverInterface $resolver, ArgumentResolverInterface $argumentResolver)
     {
         $this->matcher = $matcher;
-        $this->controllerResolver = $controllerResolver;
+        $this->controllerResolver = $resolver;
         $this->argumentResolver = $argumentResolver;
     }
 
